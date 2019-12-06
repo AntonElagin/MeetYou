eb3c212396d61d15c6187f85dd70c7ebfdcef5e4
Deiklov <romanov408g@mail.ru>
2019-12-06 13:07:26 +0300

    members_list && members_count && history && test for this

--- include/common_chat.h
+++ include/common_chat.h
@@ -72,19 +72,117 @@ public:
     }
 
     http::response<http::string_body> get() override {
+        json j = json::parse(req.body());
+        json res_body;
+        int chatid;
+        std::string search_data;
+        if (j.contains("chatid") && j.contains("search_data")) {
+            json jsonik = j.at("chatid");
+            if (jsonik.is_number())
+                chatid = j.at("chatid");
+            jsonik = j.at("search_data");
+            if (jsonik.is_string())
+                search_data = j.at("search_data");
+            if (search_data == "history")
+                res_body = history(chatid);
+            else if (search_data == "members_count")
+                res_body = members_count(chatid);
+            else if (search_data == "members_list")
+                res_body = members_list(chatid);
+            nlohmann::json body;
+            body["status"] = 200;
+            body["message"] = "OK";
+            body["content"] = res_body;
+            boost::beast::http::response<http::string_body> resp{
+                    std::piecewise_construct,
+                    std::make_tuple(std::move(body.dump())),
+                    std::make_tuple(http::status::ok, req.version())};
+            resp.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+            resp.set(http::field::content_type, "application/json");
+            resp.content_length(body.size());
+            resp.prepare_payload();
+            return resp;
+        }
         return boost::beast::http::response<http::string_body>();
     }
 
+    json history(const int chatid) {
+        std::unique_ptr<sql::PreparedStatement> stmt(conn->prepareStatement(
+                "select username nick,body,publication_date pubdate from Message, User where chat_id = ? and author_id = User.id order by publication_date asc;"));
+        stmt->setInt(1, chatid);
+        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery());
+        std::string text;
+        std::string author;
+        std::string date;
+        json j;
+        while (res->next()) {
+            text = res->getString("body");
+            author = res->getString("nick");
+            date = res->getString("pubdate");
+            std::vector<std::string> message_data = {text, author, date};
+            j.push_back(message_data);
+        }
+        return std::move(j);
+    }
+
+    json members_count(const int chatid) {
+        std::unique_ptr<sql::PreparedStatement> stmt(conn->prepareStatement(
+                "select count(user_id) quant from result_table where chat_id=?"));
+        stmt->setInt(1, chatid);
+        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery());
+        res->next();
+        int members_count = res->getInt(1);
+        json j;
+        j["members_count"] = members_count;
+        return j;
+    }
+
+    json members_list(const int chatid) {
+        std::unique_ptr<sql::PreparedStatement> stmt(conn->prepareStatement(
+                "select user_id id, username nick from result_table, User where user_id=User.id and chat_id=?"));
+        stmt->setInt(1, chatid);
+        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery());
+        std::map<int, std::string> id_nick_array;
+        while (res->next()) {
+            id_nick_array[res->getInt("id")] = res->getString("nick");
+        }
+        json j(id_nick_array);
+        return j;
+    }
+
     http::response<http::string_body> delete_() override {
-        return boost::beast::http::response<http::string_body>();
+        json j = json::parse(req.body());
+        int chatid;
+        if (j.contains("chatid")) {
+            json jsonik = j.at("chatid");
+            if (jsonik.is_number()) {
+                chatid = j.at("chatid");
+            }
+            std::unique_ptr<sql::PreparedStatement> chatStmt(conn->prepareStatement(
+                    "DELETE FROM `Chat` WHERE `id` = ?"));
+            chatStmt->setInt(1, chatid);
+            chatStmt->executeUpdate();
+            chatStmt.reset(conn->prepareStatement(
+                    "DELETE FROM `result_table` WHERE `chat_id` = ?"));
+            chatStmt->executeUpdate();
+        }
+        nlohmann::json body;
+        body["status"] = 200;
+        body["message"] = "Chat deleted";
+        boost::beast::http::response<http::string_body> resp{
+                std::piecewise_construct,
+                std::make_tuple(std::move(body.dump())),
+                std::make_tuple(http::status::ok, req.version())};
+        resp.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        resp.set(http::field::content_type, "application/json");
+        resp.content_length(body.size());
+        resp.prepare_payload();
+        return resp;
     }
 
     http::response<http::string_body> put() override {
         return boost::beast::http::response<http::string_body>();
     }
-
-
 };
-
 
 #endif //PLUS_PROJECT_COMMON_CHAT_H
--- tests/test.cpp
+++ tests/test.cpp
@@ -22,23 +22,9 @@ protected:
     void SetUp() override {
         req.version(11);
         req.method(http::verb::post);
-        req.target("/chat/history");
+        req.target("/chat/");
         req.set(http::field::user_agent, "test");
         req.set(http::field::host, "localhost");
-        req.body() = "{\n"
-                     "  \"members_list\": [\n"
-                     "    1,\n"
-                     "    2,\n"
-                     "    3,\n"
-                     "    5,\n"
-                     "    20\n"
-                     "  ],\n"
-                     "  \"admin_list\": [\n"
-                     "    4,\n"
-                     "    7\n"
-                     "  ],\n"
-                     "  \"title\": \"somechat\"\n"
-                     "}";
         req.prepare_payload();
         driver = get_driver_instance();
         shared_ptr<sql::Connection> conn(driver->connect("tcp://127.0.0.1:3306", "root", "167839"));
@@ -59,10 +45,61 @@ protected:
 
 TEST_F(QuickTest, requests) {
     int id = 2;
+    req.body() = "{\n"
+                 "  \"members_list\": [\n"
+                 "    1,\n"
+                 "    2,\n"
+                 "    3,\n"
+                 "    5,\n"
+                 "    20\n"
+                 "  ],\n"
+                 "  \"admin_list\": [\n"
+                 "    4,\n"
+                 "    7\n"
+                 "  ],\n"
+                 "  \"title\": \"somechat\"\n"
+                 "}";
     ViewChatCommon view_chat(req, con, id);
     auto kek = view_chat.post();
     boost::string_view heh = kek.body();
     ASSERT_STREQ(heh.to_string().c_str(), "{\"message\":\"OK\",\"status\":200}");
+}
+
+TEST_F(QuickTest, history) {
+    int id = 2;
+    req.body() = "{\n"
+                 "  \"chatid\": 5,\n"
+                 "  \"search_data\": \"history\"\n"
+                 "}";
+    ViewChatCommon view_chat(req, con, id);
+    auto kek = view_chat.get();
+    boost::string_view heh = kek.body();
+    ASSERT_STREQ(heh.to_string().c_str(), "{\"message\":\"OK\",\"status\":200}");
+}
+
+TEST_F(QuickTest, members_count) {
+    int id = 2;
+    req.body() = "{\n"
+                 "  \"chatid\": 1,\n"
+                 "  \"search_data\": \"members_count\"\n"
+                 "}";
+    ViewChatCommon view_chat(req, con, id);
+    auto kek = view_chat.get();
+    boost::string_view heh = kek.body();
+    ASSERT_STREQ(heh.to_string().c_str(), "{\"message\":\"OK\",\"status\":200}");
+}
+
+TEST_F(QuickTest, members_list) {
+    int id = 2;
+    req.body() = "{\n"
+                 "  \"chatid\": 1,\n"
+                 "  \"search_data\": \"members_list\"\n"
+                 "}";
+    ViewChatCommon view_chat(req, con, id);
+    auto kek = view_chat.get();
+    boost::string_view heh = kek.body();
+    ASSERT_STREQ(heh.to_string().c_str(),
+                 "{\"content\":[[2,\"Cchaddie Disdel\"],[3,\"Maxine Fanthome\"],[4,\"Elijah Attwell\"]],\"message\":\"OK\",\"status\":200}");
 }
 
 int main(int argc, char **argv) {
